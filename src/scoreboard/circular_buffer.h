8aedf8e81a300dbab13689632b1518826d681529
Sakari Tanskanen <sakari@tuxera.com>
2017-10-02 10:15:18 +0300

    Fix issues with circular buffer

--- src/scoreboard/circular_buffer.h
+++ src/scoreboard/circular_buffer.h
@@ -4,24 +4,22 @@ class CircularBuffer {
     CircularBuffer() : index(0), count(0) {}
     
     void push_item(T item) {
-        array[index + 1] = item;
         index = (index + 1) % S;
-        count++;
+        array[index] = item;
+        if (count < S) count++;
     }
 
     T pop_item() {
+        int old_index = index;
         if (count > 0) {
-            int old_index = index;
             index = index == 0 ? S - 1 : index - 1;
             count--;
-            return array[old_index];
         }
+        return array[old_index];
     }
 
-    T peak_item() {
-        if (count > 0) {
-            return array[index];
-        }
+    T peek_item() {
+        return array[index];
     }
   private:
     int index;
